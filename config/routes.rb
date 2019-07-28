3a6531f19073efa9de99fc810f3c675045958e74
ilan szenker <szenkerilan@gmail.com>
2019-07-28 15:24:24 +0300

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get "sites", to: "sites#index"
-  get "sites/:id", to: "sites#show", as: :site
-  get 'sites/new'
-  get 'sites/create'
-  get 'sites/edit'
-  get 'sites/update'
-  get 'sites/destroy'
-  root to: 'pages#home'
+  # get "sites", to: "sites#index"
+  # get "sites/:id", to: "sites#show", as: :site
+  # get 'sites/new'
+  # get 'sites/create'
+  # get 'sites/edit'
+  # get 'sites/update'
+  # get 'sites/destroy'
+  # root to: 'pages#home'
+
+  resources :sites
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
